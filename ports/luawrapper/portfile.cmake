45e2c108b070ec101c62b06c180e325efcabbbe4
Alexander Ames <Alexander.Ames@gmail.com>
2022-11-14 21:00:59 -0800

    Trying to get luawrapper example to work.

--- ports/luawrapper/portfile.cmake
+++ ports/luawrapper/portfile.cmake
@@ -1,9 +1,9 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO alexames/luawrapper
-  REF 44d4fd7db30b461578a6d59079bde409ee2b1743
+  REF e5b559eef6e54f1542749a68ed4d8b1cb7b6163d
   SHA512 c9096cc111f18791981f0845fb12cb3627c804f4ac85d54fe7f3d4d9b417e1fff1e7ea1d2b0f87e369064abe28a2bb0ea7b7453164f3609361bbda0048c389e5
-  HEAD_REF main
+  HEAD_REF experimental
 )
 
 vcpkg_configure_cmake(
@@ -11,8 +11,6 @@ vcpkg_configure_cmake(
 )
 vcpkg_install_cmake()
 vcpkg_fixup_cmake_targets()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(
   INSTALL "${SOURCE_PATH}/LICENSE"
